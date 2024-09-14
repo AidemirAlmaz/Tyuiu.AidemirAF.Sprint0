932a1efb043cfaa6175060271fe5a51b65e8ea12
Tyuiu.AidemirAF@outlook.com <almazaydemir@yandex.ru>
2024-09-14 16:50:06 +0500

    Task1

---
+++ Tyuiu.AidemirAF.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Tyuiu.AidemirAF.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Алмаз";
+            int age = 18;
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет! ");
+            Console.ReadKey();
+
+        }
+    }
+}
