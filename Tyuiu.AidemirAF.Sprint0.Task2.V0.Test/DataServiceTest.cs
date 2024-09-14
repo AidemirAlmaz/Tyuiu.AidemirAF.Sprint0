dec32885d9ae6bc697cc9e8484701bc5fbb27384
Tyuiu.AidemirAF@outlook.com <almazaydemir@yandex.ru>
2024-09-14 18:16:10 +0500

    Task2

---
+++ Tyuiu.AidemirAF.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+using Tyuiu.AidemirAF.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.AidemirAF.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            //Область создания методов тестирования, методов из библиотеки
+            var name = "Алмаз";
+            var res = DataService.GetMessage(name);
+
+            //Вызываем класс Assert и метод AreEqual
+            Assert.AreEqual("Привет..., Алмаз", res);
+        }
+    }
+}
