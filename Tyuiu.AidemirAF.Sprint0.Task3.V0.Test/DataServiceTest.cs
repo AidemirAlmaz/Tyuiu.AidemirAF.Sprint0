204aaa4b41dbf69f1ae156ab6e96b47aa37e1c4b
Tyuiu.AidemirAF@outlook.com <almazaydemir@yandex.ru>
2024-09-15 22:55:47 +0500

    Task3

---
+++ Tyuiu.AidemirAF.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+using Tyuiu.AidemirAF.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.AidemirAF.Sprint0.Task3.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+        }
+    }
+}
